343ad3a0c7d2887220429d51fbf2a4bc3948603c
halimacmuq <103187708+halimacmuq@users.noreply.github.com>
2023-03-16 14:01:15 +0300

    Update navbarToggle.js

--- navbarToggle.js
+++ navbarToggle.js
@@ -1,12 +1,7 @@
 document.getElementById("menu-toggle").addEventListener("click", function () {
   let hiddenElements = document.getElementsByClassName("hidden");
-  let hiddenDm = document.getElementsByClassName("hidden-2");
   let max = hiddenElements.length;
-  let max2 = hiddenDm.length;
   for (let i = 0; i < max; i++) {
     hiddenElements[i].classList.toggle("menu-collapse");
-  }
-  for (let j = 0; j < max2; j++) {
-    hiddenDm[j].classList.toggle("collapse-darkmode");
   }
 });
